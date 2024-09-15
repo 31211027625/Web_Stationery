899d44006f91e25c9f6547b23042a4d7c4c8cacc
31211027625 <143168942+31211027625@users.noreply.github.com>
2024-09-15 21:38:16 +0700

    Final

---
+++ Web_Stationery/views/main/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer-wrapper bg-dark text-light mt-5">
+    <div class="container p-2">
+        <div class="row row-cols-12">
+            <div class="row-center">
+                <div class="text-center"><b>THONG TIN CA NHAN</b></div>
+                <div class="text-center">Le Dinh Nhat Vu</div>
+                <div class="text-center">0945712321-ldnhatvu1304@gmail.com</div>
+            </div>
+        </div>
+    </div>
+</footer>
